f62ac22500216d93f6569e355d78005ca4cad7c1
Setu Potnis <setupotnis@Setus-MacBook-Pro.local>
2019-07-12 09:45:17 -0400

    initial commit

---
+++ realEstate-bot.py
@@ -0,0 +1,17 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+import random
+import sys
+
+def print_same_line(text):
+    sys.stdout.write('\r')
+    sys.stdout.flush()
+    sys.stdout.write(text)
+    sys.stdout.flush()
+
+class realEstateBot:
+    def __init__(self, email, password):
+        self.email = email
+        self.password = password
+        self.bot = webdriver.Chrome()
