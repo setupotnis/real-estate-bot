c50b0aab1ce974aaf1564ee98cf0e7295a98462a
Setu Potnis <setupotnis@Setus-MacBook-Pro.local>
2019-07-12 10:22:43 -0400

    added basic navigation features to search cities

--- realEstate-bot.py
+++ realEstate-bot.py
@@ -11,7 +11,26 @@ def print_same_line(text):
     sys.stdout.flush()
 
 class realEstateBot:
-    def __init__(self, email, password):
-        self.email = email
-        self.password = password
+    def __init__(self, search):
+        self.search = search
+        #self.password = password
         self.bot = webdriver.Chrome()
+
+    def closeBrowser(self):
+        self.bot.close()
+
+    def search_nav(self):
+        bot = self.bot
+        bot.get('https://www.zillow.com/')
+        time.sleep(2)
+        search = bot.find_element_by_class_name("react-autosuggest__input")
+        search.clear()
+        search.send_keys(self.search)
+        search.send_keys(Keys.RETURN)
+        time.sleep(2)
+        for_sale = bot.find_element_by_class_name("ListingButtons__Button-sc-8yz792-1")
+        for_sale.click()
+
+
+zillow = realEstateBot('mississauga')
+zillow.search_nav()
