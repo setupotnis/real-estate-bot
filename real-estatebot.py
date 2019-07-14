69fe3a33b7733f2e6caae857f41438bbec881186
Setu Potnis <setupotnis@Setus-MacBook-Pro.local>
2019-07-14 19:07:07 -0400

    fixed issue with popup by creating account and logging in

--- real-estatebot.py
+++ real-estatebot.py
@@ -7,7 +7,10 @@ import time
 import random
 import sys
 
-
+gta_cities = ['toronto, ON', 'mississauga, ON', 'hamilton, ON', 'kitchener, ON', 'waterloo, ON', 'barrie, ON', 'ajax'
+, 'cambridge, ON', 'peterborough, ON', 'gatineau, ON', 'burlington, ON', 'montreal, ON', 'niagara, ON', 'ottawa, ON', 'guelph',
+'quinte, ON', 'london, ON']
+searchbar = random.choice(gta_cities)
 
 def print_same_line(text):
     sys.stdout.write('\r')
@@ -16,17 +19,29 @@ def print_same_line(text):
     sys.stdout.flush()
 
 class realEstateBot:
-    def __init__(self, search):
+    def __init__(self, search, username, password):
         self.search = search
+        self.username = username
+        self.password = password
         self.bot = webdriver.Chrome()
     
     def closeBrowser(self):
         self.bot.close()
 
-    def city_search(self):
+    def login(self):
         bot = self.bot
         bot.get('https://www.point2homes.com/')
-        time.sleep(10)
+        time.sleep(2)
+        bot.find_element_by_id('login').click()
+        time.sleep(4)
+        username = bot.find_element_by_id('Username')
+        password = bot.find_element_by_id('Password')
+        username.clear()
+        password.clear()
+        username.send_keys(self.username)
+        password.send_keys(self.password)
+        password.send_keys(Keys.RETURN)
+        time.sleep(5)
         search = bot.find_element_by_id('listing-location')
         search.clear()
         search.send_keys(self.search)
@@ -34,33 +49,31 @@ class realEstateBot:
         search.send_keys(Keys.RETURN)
         time.sleep(5)
 
-
-    def get_prices(self):
+    def get_info(self):
         bot = self.bot
-        for i in range(1,5):
+        for i in range(1):
             bot.execute_script("window.scrollTo(0, document.body.scrollHeight);")
             time.sleep(5)
-            houses = bot.find_elements_by_class_name('button-flat-color')
-            locations = [elem.get_attribute('href') for elem in houses]
-            for location in locations:
-                bot.get(location)
-                time.sleep(5)
+            for j in range(10):
+                bot.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+                houses = bot.find_elements_by_class_name('button-flat-color')
+                locations = [elem.get_attribute('href') for elem in houses]
+                bot.find_element_by_class_name('pager-next').click()
+                for location in locations:
+                    try:
+                        URL = location
+                        headers = {'User-Agent': "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.90 Safari/537.36"}
+                        page = requests.get(URL, headers=headers)
+                        soup = BeautifulSoup(page.content, 'html.parser')
+                        address = soup.find(id='ratehub').get_text()
+                        print(address[50:-4]) 
+                    except Exception as e:
+                        continue
 
-                URL = location
-                headers = {'User-Agent': "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.90 Safari/537.36"}
-                page = requests.get(URL, headers=headers)
 
-                soup = BeautifulSoup(page.content, 'html.parser')
 
-                address = soup.find(id='ratehub').get_text()
-                print(address[50:-4]) 
 
-gta_cities = ['toronto', 'mississauga', 'hamilton', 'kitchener', 'waterloo', 'barrie', 'ajax'
-, 'cambridge', 'peterborough', 'gatineau', 'burlington', 'montreal', 'niagara', 'ottawa', 'guelph',
-'quinte', 'london']
-
-searchbar = random.choice(gta_cities)
-point2homes = realEstateBot(searchbar)
+point2homes = realEstateBot(searchbar, 'email', 'password')
 print(searchbar)
-point2homes.city_search()
-point2homes.get_prices()
+point2homes.login()
+point2homes.get_info()
